9c4b4547b5027919a75b3e6fe773a1bec0a1cdf6
Murari-77 <146162910+Murari-77@users.noreply.github.com>
2023-11-22 08:42:01 +0530

    added backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const ApiCall = axios.create({
-  baseURL: 'http://localhost:8080', // Replace with your backend URL
+  baseURL: "https://yoyo-backend-2pwg.onrender.com", // Replace with your backend URL
   // You can add other default configurations here like headers, timeout, etc.
 });
 
